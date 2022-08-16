47b4d9f3e6677ef0accc51b5a470b1de25c57853
charanmcr <charan_mcr@srkrec.edu.in>
2022-08-16 11:11:07 +0530

    ssc

--- pet-add-performance.php
+++ pet-add-performance.php
@@ -24,23 +24,21 @@ else
         $total=$score+$sport_score;
         $stat2="UPDATE `sports_marks` SET `marks`='$total' WHERE student_id='$student_id' AND sport_id='$sport_id'";
         $run2=mysqli_query($con,$stat2);
-		echo '<div class="alert alert-danger alert-dismissible fade show" role="alert">
-	  <strong>Error!</strong> Invalid login credentials. Please retry or contact your administrator.
-	  <button type="button" class="close" data-bs-dismiss="alert" aria-label="Close">
-	  <span aria-hidden="true">&times;</span>
-	  </button>
-  </div>';
       }
+	  else if(True)
+	  {
+		echo '
+        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+            <strong>Error!</strong> Invalid login credentials. Please retry or contact your administrator.
+            <button type="button" class="close" data-bs-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+            </button>
+        </div>';
+	  }
       else
       {
         $stat2="INSERT INTO sports_marks (student_id,school_id,sport_id,marks) VALUES ('$student_id','$school_id','$sport_id',$sport_score)";
         $run2=mysqli_query($con,$stat2);
-		echo '<div class="alert alert-danger alert-dismissible fade show" role="alert">
-	  <strong>Error!</strong> Invalid login credentials. Please retry or contact your administrator.
-	  <button type="button" class="close" data-bs-dismiss="alert" aria-label="Close">
-	  <span aria-hidden="true">&times;</span>
-	  </button>
-  </div>';
       }
 	  
    }
