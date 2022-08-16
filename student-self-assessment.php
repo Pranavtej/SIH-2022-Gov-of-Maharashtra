a121a56249f07f4f54095d18a339134775454527
mrchinnu <ganesh_mcr@srekrec.edu.in>
2022-08-16 09:57:36 +0530

    monkey d luffy

---
+++ student-self-assessment.php
@@ -0,0 +1,107 @@
+<?php
+include "connect.php";
+$sql="select Question from student_assessment where 1";
+$query=mysqli_query($con,$sql);
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
+        <title>Preskool - Students</title>
+		
+		<!-- Favicon -->
+        <link rel="shortcut icon" href="assets/img/favicon.png">
+	
+		<!-- Fontfamily -->
+		<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,500;0,600;0,700;1,400&display=swap">
+		
+		<!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="assets/plugins/bootstrap/css/bootstrap.min.css">
+		
+		<!-- Fontawesome CSS -->
+		<link rel="stylesheet" href="assets/plugins/fontawesome/css/fontawesome.min.css">
+		<link rel="stylesheet" href="assets/plugins/fontawesome/css/all.min.css">
+
+		<!-- Select CSS -->
+		<link rel="stylesheet" href="assets/plugins/select2/css/select2.min.css">
+		
+		<!-- Main CSS -->
+        <link rel="stylesheet" href="assets/css/style.css">
+    </head>
+    <body>
+		
+		<!-- Main Wrapper -->
+        <div class="main-wrapper">
+			
+            <?php include 'school-admin-header.php';?>
+			<?php include 'school-admin-sidebar.php';?>
+		
+				
+			<!-- Page Wrapper -->
+            <div class="page-wrapper">
+                <div class="content container-fluid">
+				
+					<!-- Page Header -->
+					<div class="page-header">
+						<div class="row align-items-center">
+							<div class="col">
+								<h5 class="page-title">Student-Self-Assessment</h5>
+								<ul class="breadcrumb">
+									<!-- <li class="breadcrumb-item"><a href="school-admin-section-list.php">Section-list</a></li>
+									<li class="breadcrumb-item active">Add Section</li> -->
+								</ul>
+							</div>
+						</div>
+					</div>
+					<!-- /Page Header -->
+<form action="" method="post">
+
+
+ 
+	<?php 
+	$i=0;
+	foreach($query as $data){
+		
+		echo
+			
+' <div class="form-group">
+<h5> '.$data['Question'].'
+ <select name="" id="cars">
+  <option value="volvo">Volvo</option>
+  <option value="saab">Saab</option>
+  <option value="mercedes">Mercedes</option>
+  <option value="audi">Audi</option>
+</select></h5>
+</div>';
+
+		}
+?>
+<div class="form-group">
+<input type="submit" value="Submit" name="submit" class="btn btn-primary"/>
+</div>
+</form>
+</div>				
+			</div>
+			<!-- /Page Wrapper -->
+			
+        </div>
+		<!-- /Main Wrapper -->
+		
+		<!-- jQuery -->
+        <script src="assets/js/jquery-3.6.0.min.js"></script>
+		
+		<!-- Bootstrap Core JS -->
+        <script src="assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+		
+		<!-- Slimscroll JS -->
+		<script src="assets/plugins/slimscroll/jquery.slimscroll.min.js"></script>
+
+		<!-- Select2 JS -->
+		<script src="assets/plugins/select2/js/select2.min.js"></script>
+
+		<!-- Custom JS -->
+		<script src="assets/js/script.js"></script>
+		
+    </body>
+</html>
