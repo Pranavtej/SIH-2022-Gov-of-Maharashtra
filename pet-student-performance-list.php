7967d63e82faa817f61c18bb88d1652d1a8158e1
charanmcr <charan_mcr@srkrec.edu.in>
2022-08-16 15:57:55 +0530

    change

--- pet-student-performance-list.php
+++ pet-student-performance-list.php
@@ -149,7 +149,7 @@ while($data = mysqli_fetch_assoc($result))
 <td>'.$date.'</td>
 <td>'.$mail.'</td>
 <td>'.$addres.'</td>
-<td><a href="pet-add-performance.php?student_id='.$std_id.'">
+<td><a href="pet-add-performance.php?student_id='.$std_id.'&cid='.$class_id.'">
 <button class="btn btn-primary">
     Add Performance 
 </button>
