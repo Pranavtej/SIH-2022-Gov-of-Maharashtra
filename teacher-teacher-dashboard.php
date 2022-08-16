6e65e0b677995aeb810fc5b687a0e781a63e0935
mrchinnu <ganesh_mcr@srekrec.edu.in>
2022-08-16 11:16:28 +0530

    mdl

--- teacher-teacher-dashboard.php
+++ teacher-teacher-dashboard.php
@@ -54,9 +54,9 @@
 		<!-- Main Wrapper -->
         <div class="main-wrapper">
 		
-			<?php include 'teacher-header.php' ?>
+			<?php include 'teacher-teacher-header.php' ?>
             			
-			<?php include 'teacher-sidebar.php' ?>
+			<?php include 'teacher-teacher-sidebar.php' ?>
 			
 			<!-- Page Wrapper -->
             <div class="page-wrapper">
