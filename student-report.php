9ee2ca992ab8e3463485d57cb4ac9af263fb0dc4
charanmcr <charan_mcr@srkrec.edu.in>
2022-08-18 22:40:14 +0530

    star update

--- student-report.php
+++ student-report.php
@@ -15,6 +15,36 @@ $sql2 = "select class,section from classes where class_id='$class_id'";
 $run2 = mysqli_query($con, $sql2);
 $run2 = mysqli_fetch_assoc($run2);
 
+$query1="select eid from exam where status=1";
+$result1=mysqli_query($con,$query1) or die(mysqli_error);
+$res = mysqli_fetch_assoc($result1);
+$eid = $res['eid'];
+
+$qu="select e.student_id as sid from exam_totals e,student s where e.eid='$eid' and e.school_id='$school_id' and e.student_id=s.student_id and s.class_id='$class_id' order by e.total desc";
+$re=mysqli_query($con,$qu);
+$var=0;
+foreach($re as $data)
+{
+	$var=$var+1;
+	if($data['sid']==$student_id)
+	{
+		$rank = $var;
+		break;
+	}
+}
+
+$qu1 = mysqli_query($con , "SELECT student_id,sum(marks) as marks from sports_marks where school_id='$school_id' GROUP BY student_id order by marks desc");
+$var3=0;
+foreach($qu1 as $data)
+{
+	$var3=$var3+1;
+	if($data['student_id']==$student_id)
+	{
+		$rank3 = $var3;
+		break;
+	}
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -120,23 +150,24 @@ $run2 = mysqli_fetch_assoc($run2);
 											<div class="col-xl-3 col-sm-6 col-12">
 												<div class="card flex-fill fb sm-box">
 													<!-- <i class="fab fa-facebook"></i> -->
-													<h6>50,095</h6>
-													<p>Likes</p>
+													<h6><?php echo $rank; ?></h6>
+													<p>Academeic Rank</p>
 												</div>
 											</div>
-											<div class="col-xl-3 col-sm-6 col-12">
-												<div class="card flex-fill twitter sm-box">
-													<!-- <i class="fab fa-twitter"></i> -->
-													<h6>48,596</h6>
-													<p>Follows</p>
-												</div>
-											</div>
-						
+
 											<div class="col-xl-3 col-sm-6 col-12">
 												<div class="card flex-fill insta sm-box">
 													<!-- <i class="fab fa-instagram"></i> -->
 													<h6>52,085</h6>
 													<p>Follows</p>
+												</div>
+											</div>
+											
+											<div class="col-xl-3 col-sm-6 col-12">
+												<div class="card flex-fill twitter sm-box">
+													<!-- <i class="fab fa-twitter"></i> -->
+													<h6><?php echo $rank3; ?></h6>
+													<p>Sports Rank</p>
 												</div>
 											</div>
 						
