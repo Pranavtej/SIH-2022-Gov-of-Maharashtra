d5e6124d266c02d257eb388f9e05ede1a896ebff
charanmcr <charan_mcr@srkrec.edu.in>
2022-08-19 02:07:12 +0530

    update

--- student-self-assesment.php
+++ student-self-assesment.php
@@ -39,7 +39,7 @@ if(isset($_POST['submit']))
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
-        <title>Parent-Student-Assessment</title>
+        <title>Student-Self-Assessment</title>
 		
 		<!-- Favicon -->
         <link rel="shortcut icon" href="assets/img/favicon.png">
