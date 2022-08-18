18b955a08a02dc6a9b912052067e0066a95cf416
charanmcr <charan_mcr@srkrec.edu.in>
2022-08-19 03:59:31 +0530

    pp

--- pet-dashboard.php
+++ pet-dashboard.php
@@ -339,7 +339,8 @@ const myChart = new Chart(ctx_2, {
                                 $re=mysqli_query($con,$qu1);
                                 $re1 = mysqli_fetch_assoc($re);
                                 echo '
-                                <span class="feed-text1"><a>'.$re1['student_name'].'</a></span>';
+                                <span class="feed-text1"><a>'.$re1['student_name'].'</a></span>
+                                <p><span>Super</span></p>';
                             }
                         ?>
                     </li>
