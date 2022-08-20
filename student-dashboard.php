202df923a14f2f530d32b6b5b2d0b7b6601a2c21
Pranavtej <pranav_mcr@srkrec.edu.in>
2022-08-20 22:01:35 +0530

    new with ganesh test

--- student-dashboard.php
+++ student-dashboard.php
@@ -384,7 +384,7 @@ else{
 														//echo json_encode($y), 
 														datasets: [{
 															label: 'MARKS SCORED',
-															data:m,
+															data:[m],
 														//echo json_encode($x),
 
 															backgroundColor: [
