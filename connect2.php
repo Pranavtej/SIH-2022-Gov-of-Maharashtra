17dfadccc220447293c3cc12f02ae6fb318df2cc
charanmcr <charan_mcr@srkrec.edu.in>
2022-08-21 19:13:23 +0530

    machine learning model 1.0

---
+++ connect2.php
@@ -0,0 +1,10 @@
+<?php
+
+$con = new mysqli('localhost','root','');
+
+if(!$con)
+{
+    die(mysqli_error());
+}
+
+?>
--- teacher-classteacher-predict-list.php
+++ teacher-classteacher-predict-list.php
@@ -1,5 +1,5 @@
 <?php
-include 'connect.php';
+include 'connect2.php';
 session_start();
 $school_id = $_SESSION['SCHOOL_ID'];
 $teacher_id = $_SESSION['TEACHER_ID'];
@@ -30,6 +30,8 @@ $class_id = $_SESSION['CLASS_ID'];
 		
 		<!-- Main CSS -->
         <link rel="stylesheet" href="assets/css/style.css">
+
+		
     </head>
     <body>
          <?php include 'teacher-sidebar.php';
@@ -49,7 +51,9 @@ $class_id = $_SESSION['CLASS_ID'];
 								</ul>
 							</div>
 							<div class="col-auto text-end float-end ms-auto">
-								<a href="#" class="btn btn-outline-primary me-2"><i class="fas fa-download"></i> Download</a>
+								<form method="post">
+									<button class="btn btn-outline-primary me-2" name="generate">Genratate</button>
+								</form>
 							</div>
 						</div>
 					</div>
@@ -61,14 +65,12 @@ $class_id = $_SESSION['CLASS_ID'];
 							<div class="card card-table">
 								<div class="card-body">
 									<div class="table-responsive">
-										<table class="table table-hover table-center mb-0 datatable">
+										<table class="table table-hover table-center mb-0">
 											<thead>
 												<tr>
 													<th>STUDENT ID</th>
 													<th>STUDENT NAME</th>
-													<th>DOB</th>
-													<th>GENDER</th>
-													<th>Email</th>
+													<th>Prediction</th>
 												</tr>
 											</thead>
 											<tbody>
@@ -77,18 +79,44 @@ $class_id = $_SESSION['CLASS_ID'];
 														{
 															$query1="SELECT s.student_id as student_id,s.school_id as school_id, s.student_name as student_name, 
 															s.date_of_birth as date_of_birth, s.gender as gender, s.email as email, s.address as address,sc.school_name as school_name,
-															c.class as class, c.section as section,
-															s.class_id as class_id FROM student s,classes c,school_info sc WHERE c.class_id=s.class_id and sc.school_id=s.school_id and s.class_id='$class_id' and s.school_id='$school_id'";
+															c.class as class, c.section as section,s.class_id as class_id FROM test2.student s,test2.classes c,test2.school_info sc WHERE
+															c.class_id=s.class_id and sc.school_id=s.school_id and s.class_id='$class_id' and s.school_id='$school_id'";
 															$run1=mysqli_query($con,$query1);
+															$j = 0;
 															while($res1=mysqli_fetch_assoc($run1))
 															{
+																$sid = $res1['student_id'];
+																$marks = array(0,0,0);
+																$query2 = "select total from test3.exam_totals where student_id='$sid' order by year asc";
+																$run2=mysqli_query($con,$query2);
+																$i = 0;
+																while($res2=mysqli_fetch_assoc($run2))
+																{
+																	$marks[$i] = $res2['total'];
+																	$i++;																	
+																}
+																echo '
+																	<script>
+																		generate('.$marks[0].','.$marks[1].','.$marks[2].');
+																		function generate(a,b,c) {
+																			var xmlhttp=new XMLHttpRequest();
+																			xmlhttp.onreadystatechange=function() {
+																				if (this.readyState==4 && this.status==200) {
+																					console.log(this.responseText);
+																					document.getElementById("'.$j.'").innerHTML=this.responseText;
+																				}
+																			}
+																			xmlhttp.open("GET","https://stdpyml.herokuapp.com/?m1="+a+"&m2=1&m3="+b+"&m4=1&m5="+c+"&m6=1",true);
+																			xmlhttp.send();
+																		}
+																	</script>
+																';
 																echo 
 																'<tr><td>'.$res1['student_id'].'</td>
-																<td><a href="teacher-student-view.php?student_id='.$res1['student_id'].'">'.$res1['student_name'].'</a></td>
-																<td>'.$res1['date_of_birth'].'</td>
-																<td>'.$res1['gender'].'</td>
-																<td>'.$res1['email'].'</td>
+																<td>'.$res1['student_name'].'</td>
+																<td><span id="'.$j.'" class="badge badge-info"></span></td>
 																</tr>';
+																$j++;
 															}
 														}
                                                     ?>
