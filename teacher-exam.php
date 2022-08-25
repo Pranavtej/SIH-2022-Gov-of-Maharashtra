d7ea622b93df37f2d17331d96ba093260226dcb5
charanmcr <charan_mcr@srkrec.edu.in>
2022-08-25 15:50:35 +0530

    pu

--- teacher-exam.php
+++ teacher-exam.php
@@ -105,6 +105,9 @@ $loc = mysqli_query($con, "SELECT question_id,question,options,loc_id,image_path
                                                         </div>
                                                         <div class="card-footer text-muted">
                                                         This is my footer
+                                                        </div>
+                                                        </div>
+                                                        </div>
                                                         </div>';
 													}
                                                 ?>
