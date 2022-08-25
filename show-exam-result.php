1ba6960bf06e0008363dcf5d3a8c94f6d80262c4
charanmcr <charan_mcr@srkrec.edu.in>
2022-08-26 01:07:40 +0530

    gg

--- show-exam-result.php
+++ show-exam-result.php
@@ -10,6 +10,7 @@ session_start();
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
+        <meta http-equiv="refresh" content="3">
         <title>Preskool - Students</title>
 		
 		<!-- Favicon -->
