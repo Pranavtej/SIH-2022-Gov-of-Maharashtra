208b68d856802307578bbbfe603f599557ad05bd
Pranavtej <pranav_mcr@srkrec.edu.in>
2022-08-26 03:12:18 +0530

    DDD

--- ltest.php
+++ ltest.php
@@ -137,7 +137,7 @@ body {
                                     </div>
                                     <div class="col-12 col-md-6 col-lg-4 d-flex" style="background-color:  #99ddff;">
                                     <div class="card flex-fill" style="background-color:  #99ddff;">
-                                    <div class="card-header" >
+                                    <div class="card-header" style="background-color:  #99ddff;">
                                    <h4> Question 2: Find the result of this addition?</h4><br>
                                     <h6>(Learning Outcome : able to count objects using numbers 1 to 9)</h6>              
                                     </div><h2>4+4= </h2><div class="card-body">
