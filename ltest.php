1ad3a9985dc1639bd0bc6cd47059e8fdaeb879f0
Pranavtej <pranav_mcr@srkrec.edu.in>
2022-08-26 01:49:04 +0530

    dd

---
+++ ltest.php
@@ -0,0 +1,260 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
+        <title>Preskool - Students</title>
+		
+		<!-- Favicon -->
+        <link rel="shortcut icon" href="assets/img/favicon.png">
+	
+		<!-- Fontfamily -->
+		<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,500;0,600;0,700;1,400&display=swap">
+		
+		<!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="assets/plugins/bootstrap/css/bootstrap.min.css">
+		
+		<!-- Fontawesome CSS -->
+		<link rel="stylesheet" href="assets/plugins/fontawesome/css/fontawesome.min.css">
+		<link rel="stylesheet" href="assets/plugins/fontawesome/css/all.min.css">
+
+		<!-- Datatables CSS -->
+		<link rel="stylesheet" href="assets/plugins/datatables/datatables.min.css">
+		
+		<!-- Main CSS -->
+        <link rel="stylesheet" href="assets/css/style.css">
+    </head>
+    <style>
+body {
+    font-family: 'Montserrat';font-size: 22px;
+}
+</style>
+	 <style>
+
+/* HIDE RADIO */
+[type=radio] { 
+  position: absolute;
+  opacity: 0;
+  width: 0;
+  height: 0;
+}
+
+/* IMAGE STYLES */
+[type=radio] + img {
+  cursor: pointer;
+  width:60px;
+}
+
+/* CHECKED STYLES */
+[type=radio]:checked + img {
+  outline: 2px solid #000000;
+  width:100px;
+}
+
+/* IMAGE STYLES */
+.mno + img {
+  cursor: pointer;
+  width:100px;
+}
+
+/* CHECKED STYLES */
+.mno:checked + img {
+  outline: 0px solid #000000;
+  width:100px;
+  background-color:#ffc7b0;
+  border-radius:100px;
+}
+
+#teams1, #teams2, #teams3, #teams4, #super
+{
+  width:100%;	
+  text-align:center;	
+}
+		
+	 </style>
+    <body>
+		
+		<!-- Main Wrapper -->
+        <div class="main-wrapper">
+		
+         
+
+			
+			<!-- Page Wrapper -->
+            
+                <div class="content container-fluid">
+				
+					<!-- Page Header -->
+					<div class="page-header">
+						<div class="row align-items-center">
+							<div class="col">
+								<h3 class="page-title" align="center">Learning Outcome Assessment Examination </h3>
+                               
+                                <h5 align="left"  >Grade : 1</h5><h5 align="right">Subject : Mathematics </h5>
+								<ul class="breadcrumb">
+									
+								</ul>
+							</div>
+							<!-- <div class="col-auto text-end float-end ms-auto">
+								<a href="#" class="btn btn-outline-primary me-2"><i class="fas fa-download"></i> Download</a>
+								<a href="add-student.html" class="btn btn-primary"><i class="fas fa-plus"></i></a>
+							</div> -->
+						</div>
+					</div>
+					<!-- /Page Header -->
+				
+                   
+                        <form action="" method="post">
+                        <div class="row">
+                            
+                                    <div class="col-12 col-md-6 col-lg-4 d-flex">
+                                    <div class="card flex-fill">
+                                    <div class="card-header">
+                                   <h4> Question 1:1. Find the shape Rectangle from Below</h4><br>
+                                    <h6>(Learning Outcome : able to count objects using numbers 1 to 9)</h6>              
+                                    </div>
+                                    <div id='q10'>
+<label id='l1'>
+  <input type="radio" name="Q10" id="no1" class="mno" value="1">
+  <img src="exam/img/1/1.png" class="hvr-wobble-to-top-right">
+</label>
+<label id='l2'>
+  <input type="radio"   name="Q10" id="no2" class="mno" value="2">
+  <img src="exam/img/1/2.png" class="hvr-wobble-to-top-right">
+</label>
+<label id='l3'>
+  <input type="radio" name="Q10" id="no3" class="mno" value="3">
+  <img src="exam/img/1/3.png" class="hvr-wobble-to-top-right">
+</label>
+<label id='l4'>
+  <input type="radio" name="Q10" id="no4" class="mno" value="4">
+  <img src="exam/img/1/4.png" class="hvr-wobble-to-top-right">
+</label>
+                                    </div>
+                                    </div>
+                                    </div>
+                                    <div class="col-12 col-md-6 col-lg-4 d-flex">
+                                    <div class="card flex-fill">
+                                    <div class="card-header">
+                                   <h4> Question 2: 2. Find the result of this addition?</h4><br>
+                                    <h6>(Learning Outcome : able to count objects using numbers 1 to 9)</h6>              
+                                    </div><h2>4+4= <input type="text" name="Q11"></h2><div class="card-body">
+                                    <p class="card-text">Answer: <input type="text" name="Q11"></p>
+                                    </div>
+                                    </div>
+                                    </div>
+                                    <div class="col-12 col-md-6 col-lg-4 d-flex">
+                                    <div class="card flex-fill">
+                                    <div class="card-header">
+                                   <h4> Question 3: Write the digit '1' in word form?</h4><br>
+                                    <h6>(Learning Outcome : able to identify number)</h6>              
+                                    </div><div class="card-body">
+                                    <p class="card-text">Answer: <input type="text" name="Q12"></p>
+                                    </div>
+                                    </div>
+                                    </div>
+                                    <div class="col-12 col-md-6 col-lg-4 d-flex">
+                                    <div class="card flex-fill">
+                                    <div class="card-header">
+                                   <h4> Question 4: Insert <,>,= : 35  __  45</h4><br>
+                                    <h6>(Learning Outcome : able to distinguish between larger and smaller numbers)</h6>              
+                                    </div><div class="card-body">
+                                    <p class="card-text">Answer: <input type="text" name="Q15"></p>
+                                    </div>
+                                    </div>
+                                    </div>
+                                    <div class="col-12 col-md-6 col-lg-4 d-flex">
+                                    <div class="card flex-fill">
+                                    <div class="card-header">
+                                   <h4> Question 5: Insert <,>,= : 55 __  45</h4><br>
+                                    <h6>(Learning Outcome : able to distinguish between larger and smaller numbers)</h6>              
+                                    </div><div class="card-body">
+                                    <p class="card-text">Answer: <input type="text" name="Q16"></p>
+                                    </div>
+                                    </div>
+                                    </div>
+                                    <div class="col-12 col-md-6 col-lg-4 d-flex">
+                                    <div class="card flex-fill">
+                                    <div class="card-header">
+                                   <h4> Question 6: Add : 9 + 7 = __</h4><br>
+                                    <h6>(Learning Outcome : able to add,subract and multiply digits)</h6>              
+                                    </div><div class="card-body">
+                                    <p class="card-text">Answer: <input type="text" name="Q17"></p>
+                                    </div>
+                                    </div>
+                                    </div>
+                                    <div class="col-12 col-md-6 col-lg-4 d-flex">
+                                    <div class="card flex-fill">
+                                    <div class="card-header">
+                                   <h4> Question 7: Subract : 5 - 4 = __</h4><br>
+                                    <h6>(Learning Outcome : able to add,subract and multiply digits)</h6>              
+                                    </div><div class="card-body">
+                                    <p class="card-text">Answer: <input type="text" name="Q18"></p>
+                                    </div>
+                                    </div>
+                                    </div>
+                                    <div class="col-12 col-md-6 col-lg-4 d-flex">
+                                    <div class="card flex-fill">
+                                    <div class="card-header">
+                                   <h4> Question 8: Multiply: 2 x 2 = __</h4><br>
+                                    <h6>(Learning Outcome : able to add,subract and multiply digits)</h6>              
+                                    </div><div class="card-body">
+                                    <p class="card-text">Answer: <input type="text" name="Q19"></p>
+                                    </div>
+                                    </div>
+                                    </div>
+                                    <div class="col-12 col-md-6 col-lg-4 d-flex">
+                                    <div class="card flex-fill">
+                                    <div class="card-header">
+                                   <h4> Question 9: How do you write TWELEVE using digits?</h4><br>
+                                    <h6>(Learning Outcome : able to identify number)</h6>              
+                                    </div><div class="card-body">
+                                    <p class="card-text">Answer: <input type="text" name="Q20"></p>
+                                    </div>
+                                    </div>
+                                    </div>
+                                    <div class="col-12 col-md-6 col-lg-4 d-flex">
+                                    <div class="card flex-fill">
+                                    <div class="card-header">
+                                   <h4> Question 10: Identify the number from below?</h4><br>
+                                    <h6>(Learning Outcome : able to identify number)</h6>              
+                                    </div><h6>a)A  b)?  c)5</h6><div class="card-body">
+                                    <p class="card-text">Answer: <input type="text" name="Q21"></p>
+                                    </div>
+                                    </div>
+                                    </div>                     
+                    </div>	
+                    			
+				</div>
+                <input type="submit" class="btn btn-primary" name="give">
+                </form>	
+
+				<!-- Footer -->
+				<!-- <footer>
+					<p>Copyright © 2020 Dreamguys.</p>					
+				</footer> -->
+				<!-- /Footer -->				
+			</div>
+			<!-- /Page Wrapper -->
+			
+        </div>
+		<!-- /Main Wrapper -->
+		
+		<!-- jQuery -->
+        <script src="assets/js/jquery-3.6.0.min.js"></script>
+		
+		<!-- Bootstrap Core JS -->
+        <script src="assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+		
+		<!-- Slimscroll JS -->
+		<script src="assets/plugins/slimscroll/jquery.slimscroll.min.js"></script>
+		
+		<!-- Datatables JS -->
+		<script src="assets/plugins/datatables/datatables.min.js"></script>
+		
+		<!-- Custom JS -->
+		<script src="assets/js/script.js"></script>
+    </body>
+</html>
