6c1a0686172d5886d1ddf8de2ca74268647f3e6f
charanmcr <charan_mcr@srkrec.edu.in>
2022-08-26 12:41:09 +0530

    aa

--- exam-result-view.php
+++ exam-result-view.php
@@ -105,7 +105,7 @@ $run = mysqli_fetch_array($query);
                                                             <td>'.$d['loc'].'</td>
 															<td>Class 1</td>
 															<td>Mathematics</td>
-                                                            <td><a herf="view-std.php?sid='.$student_id.'&loc_id='.$d['locid'].'&eid=E10">'.$d['credits'].'/5</a></td>
+                                                            <td><a href="view-std.php?sid='.$student_id.'&loc_id='.$d['locid'].'&eid=E10">'.$d['credits'].'/5</a></td>
 															<td>'.$per.'%</td>
                                                         </tr>';
                                                     }													
