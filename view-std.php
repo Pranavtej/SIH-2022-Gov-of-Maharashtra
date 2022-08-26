293f36ef562d050bae1e5a9bbc1340d10df2dc36
charanmcr <charan_mcr@srkrec.edu.in>
2022-08-26 12:52:52 +0530

    a

--- view-std.php
+++ view-std.php
@@ -97,7 +97,7 @@ $loc = mysqli_query($con, "SELECT question_id,question,options,loc_id,image_path
                                         echo '<img src="exam/img/'.$run1['image_path'].'" alt="image not loaded" class="card-img">';
                                     }    
                                     echo '<div class="card-body">
-                                    <p class="card-text">Student Answer: <input type="text" disabled>'.$a['answer'].'</p>
+                                    <p class="card-text">Student Answer: '.$a['answer'].'</p>
                                     </div>
                                     </div>
                                     </div>';
