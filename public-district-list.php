269b9fc33636dd82e6c05d7bd96bf0a742bdd773
mrchinnu <ganesh_mcr@srekrec.edu.in>
2022-08-26 15:41:08 +0530

    mdl

--- public-district-list.php
+++ public-district-list.php
@@ -1,5 +1,13 @@
 <?php
-include 'connect.php';?>
+include 'connect.php';
+$sql1=mysqli_query($con,"select count(student_id) as total from student where school_id=ANY(select school_id from school_info where district_id=) group by school_id");
+
+foreach($sql1 as $data)
+{
+   
+   $x[]=$data['total'];
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -98,6 +106,54 @@ include 'connect.php';?>
 					</div>					
 				</div>			
 			</div>
+           
+  
+  <div class="card-body">
+                                        <canvas id="acscore"></canvas>
+                                            <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+                                            <script>
+												
+												const ctx = document.getElementById('acscore');
+												const myChart = new Chart(ctx, {
+													type: 'pie',
+													data: {
+														labels: <?php echo json_encode($y) ?>,
+														//echo json_encode($y), 
+														datasets: [{
+															label: '',
+															data:<?php echo json_encode($x)?>,
+														//echo json_encode($x),
+                                                        // labelLinks: ["sample-school.php?sid=SC0001","sample-school.php?sid=SC0002"],
+															backgroundColor: [
+																'rgba(255, 99, 132, 0.2)',
+																'rgba(54, 162, 235, 0.2)',
+																'rgba(255, 206, 86, 0.2)',
+																'rgba(75, 192, 192, 0.2)',
+																'rgba(153, 102, 255, 0.2)',
+																'rgba(255, 159, 64, 0.2)'
+															],
+															borderColor: [
+																'rgba(255, 99, 132, 1)',
+																'rgba(54, 162, 235, 1)',
+																'rgba(255, 206, 86, 1)',
+																'rgba(75, 192, 192, 1)',
+																'rgba(153, 102, 255, 1)',
+																'rgba(255, 159, 64, 1)'
+															],
+															borderWidth: 1
+														}]
+													},
+													options: {
+														//maintainAspectRatio: true,
+														indexAxis : 'y',
+														scales: {
+															y: {
+																beginAtZero: true
+															}
+														}
+													}
+												});
+</script>
 			<!-- /Page Wrapper -->
 			
         </div>
