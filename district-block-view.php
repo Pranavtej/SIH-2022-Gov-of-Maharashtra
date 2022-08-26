7bc7ab1615ad59757ea045b72f6e8c7a28db538c
mrchinnu <ganesh_mcr@srekrec.edu.in>
2022-08-26 07:43:53 +0530

    mdl

--- district-block-view.php
+++ district-block-view.php
@@ -1,6 +1,6 @@
 <?php
 include "connect.php";
-$bid=$_GET["bid"];
+$bid=$_GET["id"];
 $sql1=mysqli_query($con,"select count(student_id) as total from student where school_id=ANY(select school_id from school_info where block_id='$bid') group by school_id");
 
 foreach($sql1 as $data)
@@ -23,13 +23,6 @@ foreach($sql3 as $data2)
 <!DOCTYPE html>
 <html lang="en">
     <head>
-    <style>
-  #pscore {
-    display: inline-block;
-    position: relative;
-    width: 80%;
-  }
-</style>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
         <title>State-Official-Dashboard</title>
@@ -63,7 +56,7 @@ foreach($sql3 as $data2)
 					<div class="page-header">
 						<div class="row">
 							<div class="col-sm-12">
-								<h3 class="page-title">School wise NO.OF Students and Pass Percentage of Each School <?php echo json_encode($y) ?></h3>
+								<h3 class="page-title">School wise NO.OF Students and Pass Percentage of Each School </h3>
 								<ul class="breadcrumb">
 									<li class="breadcrumb-item active"></li>
 								</ul>
@@ -131,6 +124,7 @@ foreach($sql3 as $data2)
 													},
 													options: {
 														//maintainAspectRatio: true,
+														indexAxis : 'y',
 														scales: {
 															y: {
 																beginAtZero: true
@@ -159,7 +153,7 @@ foreach($sql3 as $data2)
                                             <script>
 												// m = JSON.parse(localStorage.m);
 												const ctx1 = document.getElementById('pscore');
-                                                var ctxP = ctx1.getContext('2d');
+                                                // var ctxP = ctx1.getContext('2d');
 												const myChart1 = new Chart(ctx1, {
 													type: 'bar',
 													data: {
@@ -201,28 +195,28 @@ foreach($sql3 as $data2)
 														}
 													}
 												});
-//                                                 myChart1.canvas.addEventListener('click',(e) => {
-//                                                 clickableScales(myChart1,e)
-//                                                    });
-//                                                    function clickableScales(chart,click){
-//     const{ctx,canvas,scales:{x,y}}=chart;
-//    const top=y.top
-//   const left=y.left
-//   const right=y.right
-//   const bottom=y.bottom
-//   const height=y.height/ y.ticks.length;
-//   // mouse coordinates
-//   let rect=canvas.getBoundingClientRect();
-//   const xCoor=click.clientX-rect.left;
-//   const yCoor=click.clientY-rect.top;
-//   for(let i=0;i < y.ticks.length;i++){
-//     if(xCoor>=left && xCoor<=right && yCoor>=top+(height*i)&&
-//        yCoor<=top+height+(height*i)){
-//     window.open(chart.data.datasets[0].labelLinks[i]);
+                                                myChart1.canvas.addEventListener('click',(e) => {
+                                                clickableScales(myChart1,e)
+                                                   });
+                                                   function clickableScales(chart,click){
+    const{ctxp,canvas,scales:{x,y}}=chart;
+   const top=y.top
+  const left=y.left
+  const right=y.right
+  const bottom=y.bottom
+  const height=y.height/ y.ticks.length;
+  // mouse coordinates
+  let rect=canvas.getBoundingClientRect();
+  const xCoor=click.clientX-rect.left;
+  const yCoor=click.clientY-rect.top;
+  for(let i=0;i < y.ticks.length;i++){
+    if(xCoor>=left && xCoor<=right && yCoor>=top+(height*i)&&
+       yCoor<=top+height+(height*i)){
+    window.open(chart.data.datasets[0].labelLinks[i]);
       
-// }
-//    }
-// }
+}
+   }
+}
 
 											</script>
 										</div>
---
+++ sample-dashboard.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
+        <title>State-Official-Dashboard</title>
+		
+		<!-- Favicon -->
+        <link rel="shortcut icon" href="assets/img/favicon.png">
+		
+		<!-- Fontfamily -->
+		<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,500;0,600;0,700;1,400&display=swap">
+		
+		<!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="assets/plugins/bootstrap/css/bootstrap.min.css">
+		
+		<!-- Fontawesome CSS -->
+		<link rel="stylesheet" href="assets/plugins/fontawesome/css/fontawesome.min.css">
+		<link rel="stylesheet" href="assets/plugins/fontawesome/css/all.min.css">
+		
+		<!-- Main CSS -->
+        <link rel="stylesheet" href="assets/css/style.css">
+    </head>
+    <body>
+	
+		<!-- Main Wrapper -->
+        <div class="main-wrapper">
+        <div class="content container-fluid">
+					<!-- Page Header -->
+					<div class="page-header">
+						<div class="row">
+							<div class="col-sm-12">
+								<h3 class="page-title">Public Dashboard</h3>
+								<ul class="breadcrumb">
+									<li class="breadcrumb-item active">Dashboard</li>
+								</ul>
+							</div>
+                            <select name="year" id="year" class="form-control form-select" required onchange="redirect()">
+																			<option value="">Academic Year</option>
+																			<option value="">Academic Year</option>
+                                                                            <option value="">Academic Year</option>
+</select>
+						</div>
+					</div>
+
+                    
+
+                    
+			<!-- Page Wrapper -->
+            <div class="page-wrapper">
+			
+                
+				
+					<!-- Overview Section -->
+					
+		<!-- /Main Wrapper -->
+		
+		<!-- jQuery -->
+        <script src="assets/js/jquery-3.6.0.min.js"></script>
+		
+		<!-- Bootstrap Core JS -->
+        <script src="assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+		
+		<!-- Slimscroll JS -->
+		<script src="assets/plugins/slimscroll/jquery.slimscroll.min.js"></script>
+		
+		<!-- Chart JS -->
+		<script src="assets/plugins/apexchart/apexcharts.min.js"></script>
+		<script src="assets/plugins/apexchart/chart-data.js"></script>
+
+		<!-- Custom JS -->
+		<script src="assets/js/script.js"></script>
+		
+    </body>
+</html>
